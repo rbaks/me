560a01485d43bc9b66ce5a356a5b4d4ee89ef805
rbaks <andri.bakoson@gmail.com>
2022-03-13 21:52:07 +0300

    Add contenlayer config and scripts folder

---
+++ scripts/generate-rss.mjs
@@ -0,0 +1,31 @@
+import { writeFileSync } from 'fs';
+import RSS from 'rss';
+import { allBlogs } from '../.contentlayer/generated/allBlogs.mjs';
+
+const siteMetadata = {
+  author: {
+    name: 'Andrianina Rabakoson'
+  },
+  siteUrl: 'https://andr.is-a.dev'
+}
+
+async function generate() {
+  const feed = new RSS({
+    title: `${siteMetadata.author.name}`,
+    site_url: `${siteMetadata.siteUrl}`,
+    feed_url: `${siteMetadata.siteUrl}/feed.xml`
+  });
+
+  allBlogs.map((post) => {
+    feed.item({
+      title: post.title,
+      url: `${siteMetadata.siteUrl}/blog/${post.slug}`,
+      date: post.publishedAt,
+      description: post.summary
+    });
+  });
+
+  writeFileSync('./public/feed.xml', feed.xml({ indent: true }));
+}
+
+generate();
