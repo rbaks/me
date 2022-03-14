258f64db8356fd431a2163ea3a12eb7c41d8946d
rbaks <andri.bakoson@gmail.com>
2022-03-14 23:49:07 +0300

    Add image zoom feature

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
 
 const Image = ({ src, alt, width, height, className }) => {
   return (
-    <img
-      src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}${src}`}
-      alt={alt}
-      width={width}
-      height={height}
-      className={className}
-    ></img>
+    <>
+      <Zoom>
+        <img
+          src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}${src}`}
+          alt={alt}
+          width={width}
+          height={height}
+          className={className}
+        ></img>
+      </Zoom>
+    </>
   );
 };
 
