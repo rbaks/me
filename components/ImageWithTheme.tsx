7bfa7459d4c4387523ad72563ab35fdfaf7a8e32
rbaks <andri.bakoson@gmail.com>
2022-03-15 16:44:02 +0300

    Next blog drafted

--- components/ImageWithTheme.tsx
+++ components/ImageWithTheme.tsx
@@ -1,13 +1,19 @@
 import { useTheme } from 'next-themes';
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
 
 export default function ImageWithTheme(props) {
   const { theme } = useTheme();
 
   return (
-    <img
-      alt={props.alt}
-      src={theme === 'light' ? props.light : props.dark}
-      {...props}
-    />
+    <>
+      <Zoom>
+        <img
+          alt={props.alt}
+          src={theme === 'light' ? props.light : props.dark}
+          {...props}
+        />
+      </Zoom>
+    </>
   );
 }
